7dfe2fc2875c3590a7172db49067aef9017ec8e8
Saravanakumar <saravanakumar.pandi@chainsys.com>
2022-06-07 14:21:18 +0530

    bestPractics

---
+++ src/com/chainsys/bestPractics/dao/ConnectionManager.java
@@ -0,0 +1,10 @@
+package com.chainsys.bestPractics.dao;
+
+public class ConnectionManager {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/chainsys/bestPractics/utils/Main.java
@@ -0,0 +1,10 @@
+package com.chainsys.bestPractics.utils;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hi man");
+	}
+
+}
